31582aa4e3140538a635d34a267e4dd536a9f1f8
Jahimees <antonovich.01@mail.ru>
2023-07-14 14:44:30 +0300

    Initial commit

---
+++ src/main/java/com/example/cpcore/CpCoreApplication.java
@@ -0,0 +1,13 @@
+package com.example.cpcore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CpCoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CpCoreApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/cpcore/CpCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cpcore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CpCoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
